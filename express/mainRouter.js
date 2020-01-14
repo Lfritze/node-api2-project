58dec9faf98a1f307b51545de710784d9346ba17
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-01-14 16:56:11 -0500

    successfully sent get for res.send test

--- express/mainRouter.js
+++ express/mainRouter.js
@@ -6,4 +6,20 @@ const db = require("../data/db.js");
 
 router.use(express.json());
 
+// GET to return array of data
+
+router.get("/", (req, res) => {
+  console.log(req.query);
+  db.find(req.query)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      console.log("Error in the GET", error);
+      res.status(500).json({
+        message: "Error Retrieving Data...see router.get"
+      });
+    });
+});
+
 module.exports = router;
